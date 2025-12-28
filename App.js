c6cf1d5c72a3a9ceda56c34e23066ee5a36443b1
Musab68 <musaboz109@gmail.com>
2025-12-28 12:35:53 +0300

    feat: setup stack navigation for profiles

---
+++ App.js
@@ -0,0 +1,27 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfilesListScreen from './screens/ProfilesListScreen';
+import ProfileDetailScreen from './screens/ProfileDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Liste Ekranı */}
+        <Stack.Screen 
+          name="Profiles" 
+          component={ProfilesListScreen} 
+          options={{ title: 'Profiles Directory' }}
+        />
+        {/* Detay Ekranı */}
+        <Stack.Screen 
+          name="ProfileDetail" 
+          component={ProfileDetailScreen} 
+          options={{ title: 'Profile Details' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
